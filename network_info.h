7e05f6d766dc9a09221835e2ced7df6b8169dedb
Tom Holz <tom.holz@gmail.com>
2023-10-02 13:21:10 -0700

    Fixed macro definition warning
    MSVC defines the _CONCAT macro, so redefining it here was causing warnings.

--- network_info.h
+++ network_info.h
@@ -2,10 +2,15 @@
 
 #include "version_defines.h"
 
+#include <boost/predef.h>
+
 #define _STRINGIFY(x)                     #x
 #define STRINGIFY(x)                      _STRINGIFY(x)
+
+#ifndef BOOST_COMP_MSVC
 #define _CONCAT(x,y)                      x##y
 #define CONCAT(x,y)                       _CONCAT(x,y)
+#endif
 
 //#define USER_AGENT                        STRINGIFY(CONCAT(CONCAT(Acquisition/, VER_STR), (contact: tom.holz@gmail.com)))
 #define USER_AGENT                        VER_PRODUCTNAME_STR "/" VER_STR " (contact: tom.holz@gmail.com)"
