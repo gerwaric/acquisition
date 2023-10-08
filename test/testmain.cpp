b0868e60eeb6c45a3dd12c4dd4ca3708be2e52f4
Tom Holz <tom.holz@gmail.com>
2023-10-08 10:54:30 -0700

    back to the original logging
    I should not have committed extra logging messages in the first place.

--- test/testmain.cpp
+++ test/testmain.cpp
@@ -3,8 +3,6 @@
 #include <QTest>
 #include <memory>
 
-#include "QsLog.h"
-
 #include "porting.h"
 #include "testitem.h"
 #include "testitemsmanager.h"
@@ -19,28 +17,9 @@ int test_main() {
 	QLocale::setDefault(QLocale::C);
 	std::setlocale(LC_ALL, "C");
 
-	QLOG_INFO() << "------------------------------------------------------------------------------";
-	QLOG_INFO() << "TestItem starting";
 	TEST(TestItem);
-	QLOG_INFO() << "TestItem returned" << result;
-
-	QLOG_INFO() << "------------------------------------------------------------------------------"; 
-	QLOG_INFO() << "TestShop starting";
 	TEST(TestShop);
-	QLOG_INFO() << "TestShop returned" << result;
-
-	QLOG_INFO() << "------------------------------------------------------------------------------";
-	QLOG_INFO() << "TestUtil starting";
 	TEST(TestUtil);
-	QLOG_INFO() << "TestUtil returned" << result;
-
-	QLOG_INFO() << "------------------------------------------------------------------------------";
-	QLOG_INFO() << "TestUtil starting";
 	TEST(TestItemsManager);
-	QLOG_INFO() << "TestUtil returned" << result;
-
-	result = (result != 0) ? -1 : 0;
-	QLOG_INFO() << "------------------------------------------------------------------------------";
-	QLOG_INFO() << "Final result is" << result;
-	return result;
+	return (result != 0) ? -1 : 0;
 }
