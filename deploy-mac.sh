544e1ea9404fb89078ede19880cc7e45ca8d52e9
GERWARIC <gerwaric@gmail.com>
2023-10-31 19:14:31 -0700

    Update for Qt 6

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MACDEPLOYQT=~/Qt/5.15.10/universal/bin/macdeployqt
+MACDEPLOYQT=~/Qt/6.5.3/macos/bin/macdeployqt
 
-TARGET=../build-acquisition-Qt_5_15_10_for_macOS-Release/acquisition.app
+TARGET=../build-acquisition-Qt_6_5_3_for_macOS-Release/acquisition.app
 
 $MACDEPLOYQT $TARGET -dmg -appstore-compliant
