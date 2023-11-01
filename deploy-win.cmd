3dcc67415a23f8fdf26653343db7e48221bc6413
GERWARIC <gerwaric@gmail.com>
2023-10-31 19:21:10 -0700

    Update for Qt 6

--- deploy-win.cmd
+++ deploy-win.cmd
@@ -1,10 +1,9 @@
 echo -------------------------------------------------------------
 
-set "WINDEPLOYQT=C:\Qt\5.15.2\msvc2019_64\bin\windeployqt.exe"
-set "OPENSSL=C:\Program Files\OpenSSL-Win64"
+set "WINDEPLOYQT=C:\Qt\6.5.3\msvc2019_64\bin\windeployqt.exe"
 set "ISCC=c:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 
-set "BUILD_DIR=..\build-acquisition-Desktop_Qt_5_15_2_MSVC2019_64bit-Release"
+set "BUILD_DIR=..\build-acquisition-Desktop_Qt_6_5_3_MSVC2019_64bit-Release"
 
 set "DEPLOY_DIR=.\deploy";
 
@@ -12,8 +11,6 @@ rmdir /S /Q "%DEPLOY_DIR%"
 mkdir "%DEPLOY_DIR%"
 
 copy "%BUILD_DIR%\release\acquisition.exe" "%DEPLOY_DIR%"
-copy "%OPENSSL%\bin\libcrypto-1_1-x64.dll" "%DEPLOY_DIR%"
-copy "%OPENSSL%\bin\libssl-1_1-x64.dll" "%DEPLOY_DIR%"
 
 "%WINDEPLOYQT%" "%DEPLOY_DIR%\acquisition.exe" --release --no-compiler-runtime --dir "%DEPLOY_DIR%"
 
