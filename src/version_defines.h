19cea05ee349d5a1dc418ffd42bb2e1fde8b4a58
GERWARIC <gerwaric@gmail.com>
2023-11-08 20:33:38 -0800

    Bump versions for v0.10.1

--- src/version_defines.h
+++ src/version_defines.h
@@ -8,12 +8,12 @@
 //The program will look to this file in the GitHub repo, and compare it to
 //the VER_CODE in the current installation. If the number in the version.txt
 //file is greater than VER_CODE, then the updater will notify the user.
-#define VERSION_CODE                54
+#define VERSION_CODE                55
 
 // These are used by the Inno 6 installer script.
 #define APP_NAME                    "acquisition"
-#define APP_VERSION                 "0.10.0"
-#define APP_VERSION_STRING          "v0.10.0"
+#define APP_VERSION                 "0.10.1"
+#define APP_VERSION_STRING          "v0.10.1"
 #define APP_PUBLISHER               "GERWARIC"
 #define APP_PUBLISHER_EMAIL         "gerwaric@gmail.com"
 #define APP_URL                     "https://github.com/gerwaric/acquisition"
