be7c1bb50e30a93d8edb70a3f964b101cfa66125
GERWARIC <gerwaric@gmail.com>
2023-11-12 08:01:05 -0800

    Bump version for buyout fix

--- src/version_defines.h
+++ src/version_defines.h
@@ -5,8 +5,8 @@
 //the VER_CODE in the current installation. If the number in the version.txt
 //file is greater than VER_CODE, then the updater will notify the user
 #define VER_CODE                    53
-#define VER_STR                     "v0.9.11-beta.1"
-#define VER_FILEVERSION             0,9,10,0
+#define VER_STR                     "v0.9.11"
+#define VER_FILEVERSION             0,9,11,0
 #define VER_FILEVERSION_STR         "0.9.11.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
