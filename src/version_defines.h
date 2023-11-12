5fcfe55114c6eda2c30f6e5bd01ea64b0672d6fc
GERWARIC <gerwaric@gmail.com>
2023-11-12 13:22:17 -0800

    Bump version
    Bump version for v0.10.2.
    
    Go back to using #pragma once for version_defines.h, since Inno 6 only reports a warning, but continues to process the file.

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,14 +1,10 @@
-// We can't use the `#pragma once` directive here, because this
-// file is read by Inno 6 Setup, and that program can't handle
-// the pragma. So we are left with old-school include guards.
-#ifndef ACQUISITION_VERSION_DEFINES_H
-#define ACQUISITION_VERSION_DEFINES_H
+#pragma once
 
 //Make sure to update version.txt file to match the "VER_CODE" value
 //The program will look to this file in the GitHub repo, and compare it to
 //the VER_CODE in the current installation. If the number in the version.txt
 //file is greater than VER_CODE, then the updater will notify the user.
-#define VERSION_CODE                55
+#define VERSION_CODE                56
 
 // These are used by the Inno 6 installer script.
 #define APP_NAME                    "acquisition"
@@ -21,5 +17,3 @@
 // These control if this build will expire.
 #define TRIAL_VERSION               0
 #define TRIAL_EXPIRATION_DAYS       0
-
-#endif
