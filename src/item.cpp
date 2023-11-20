8b3f191212f925326794229526a2e3e39345cd3f
GERWARIC <gerwaric@gmail.com>
2023-11-20 11:21:32 -0800

    Refactor replacement map
    No material differnce. The old format kept being indented in really odd ways by both Visual Studio and Qt Creator.

--- src/item.cpp
+++ src/item.cpp
@@ -36,27 +36,37 @@
 #include "itemlocation.h"
 #include "itemcategories.h"
 
+static const Item::CategoryReplaceMap replace_map_0_({
+	{"Divination", "Divination Cards"},
+	{"QuestItems", "Quest Items"} });
+
+static const Item::CategoryReplaceMap replace_map_1_({
+	{"BodyArmours", "Body"},
+	{"VaalGems", "Vaal"},
+	{"AtlasMaps", "2.4"},
+	{"act4maps", "2.0"},
+	{"OneHandWeapons", "1Hand"},
+	{"TwoHandWeapons", "2Hand"} });
+
+static const Item::CategoryReplaceMap replace_map_2_({
+	{"OneHandAxes", "Axes"},
+	{"OneHandMaces", "Maces"},
+	{"OneHandSwords", "Swords"},
+	{"TwoHandAxes", "Axes"},
+	{"TwoHandMaces", "Maces"},
+	{"TwoHandSwords", "Swords"} });
+
 const std::array<Item::CategoryReplaceMap, Item::k_CategoryLevels> Item::replace_map_ = {
+
 	// Category hierarchy 0 replacement map
-	Item::CategoryReplaceMap({
-		{"Divination", "Divination Cards"},
-		{"QuestItems", "Quest Items"}}),
-		// Category hierarchy 1 replacement map
-		Item::CategoryReplaceMap({
-			{"BodyArmours", "Body"},
-			{"VaalGems", "Vaal"},
-			{"AtlasMaps", "2.4"},
-			{"act4maps", "2.0"},
-			{"OneHandWeapons", "1Hand"},
-			{"TwoHandWeapons", "2Hand"}}),
-			// Category hierarchy 2 replacement map
-			Item::CategoryReplaceMap({
-				{"OneHandAxes", "Axes"},
-				{"OneHandMaces", "Maces"},
-				{"OneHandSwords", "Swords"},
-				{"TwoHandAxes", "Axes"},
-				{"TwoHandMaces", "Maces"},
-				{"TwoHandSwords", "Swords"}})
+	replace_map_0_,
+
+	// Category hierarchy 1 replacement map
+	replace_map_1_,
+			
+	// Category hierarchy 2 replacement map
+	replace_map_2_
+
 };
 
 const std::vector<std::string> ITEM_MOD_TYPES = {
