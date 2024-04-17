c7b6b48376ae30880d698626562cdd47949a5915
GERWARIC <gerwaric@gmail.com>
2024-04-17 07:48:39 +0200

    Updated to work with cmake

--- deploy-win.cmd
+++ deploy-win.cmd
@@ -3,14 +3,14 @@ echo -------------------------------------------------------------
 set "WINDEPLOYQT=C:\Qt\6.5.3\msvc2019_64\bin\windeployqt.exe"
 set "ISCC=c:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 
-set "BUILD_DIR=..\build-acquisition-Desktop_Qt_6_5_3_MSVC2019_64bit-Release"
+set "BUILD_DIR=.\build\Desktop_Qt_6_5_3_MSVC2019_64bit-Release"
 
 set "DEPLOY_DIR=.\deploy";
 
 rmdir /S /Q "%DEPLOY_DIR%"
 mkdir "%DEPLOY_DIR%"
 
-copy "%BUILD_DIR%\release\acquisition.exe" "%DEPLOY_DIR%"
+copy "%BUILD_DIR%\acquisition.exe" "%DEPLOY_DIR%"
 
 "%WINDEPLOYQT%" "%DEPLOY_DIR%\acquisition.exe" --release --no-compiler-runtime --dir "%DEPLOY_DIR%"
 
