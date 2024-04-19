eb88fb79235c1adc39c51f70efc702012345d01b
GERWARIC <gerwaric@gmail.com>
2024-04-19 23:47:40 +0200

    Add SearchComboBox for use in the search panel
    This is a customized combo box that has a delayed autocomplete, faster tooltip display, and drawn the dropdown popup as wide as needed to display possible options.

---
+++ src/searchcombobox.cpp
@@ -0,0 +1,71 @@
+#include "searchcombobox.h"
+
+#include <QAbstractItemModel>
+#include <QAbstractItemView>
+#include <QLineEdit>
+
+// Set the width of the popup view to the width of the completer options
+void SearchComboCompleter::complete(const QRect& rect) {
+	if (popup() == nullptr) {
+		return;
+	};
+	const int rows = completionModel()->rowCount();
+	const int width = (rows > 0) ? popup()->sizeHintForColumn(0) : 0;
+	popup()->setMinimumWidth(width);
+	QCompleter::complete(rect);
+}
+
+// Return a custom shortened dalay for the combox box hover tooltip
+int SearchComboStyle::styleHint(StyleHint hint, const QStyleOption* option, const QWidget* widget, QStyleHintReturn* returnData) const {
+	if (hint == QStyle::SH_ToolTip_WakeUpDelay) {
+		return TOOLTIP_DELAY_MSEC;
+	} else {
+		return QProxyStyle::styleHint(hint, option, widget, returnData);
+	};
+}
+
+SearchComboBox::SearchComboBox(QAbstractItemModel* model, QWidget* parent) :
+	QComboBox(parent),
+	completer_(model, this)
+{
+	setEditable(true);
+	setModel(model);
+	setCompleter(nullptr);
+	setInsertPolicy(QComboBox::NoInsert);
+	setStyle(new SearchComboStyle(style()));
+
+	view()->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+
+	completer_.setCompletionMode(QCompleter::PopupCompletion);
+	completer_.setFilterMode(Qt::MatchContains);
+	completer_.setCaseSensitivity(Qt::CaseInsensitive);
+	completer_.setModelSorting(QCompleter::CaseInsensitivelySortedModel);
+	completer_.setWidget(this);
+
+	connect(this, &QComboBox::editTextChanged,
+		this, &SearchComboBox::OnTextEdited);
+	
+	connect(&edit_timer_, &QTimer::timeout,
+		this, &SearchComboBox::OnEditTimeout);
+	
+	connect(&completer_, QOverload<const QString&>::of(&QCompleter::activated),
+		this, &SearchComboBox::OnCompleterActivated);
+}
+
+void SearchComboBox::OnTextEdited() {
+	edit_timer_.start(350);
+}
+
+void SearchComboBox::OnEditTimeout() {
+	edit_timer_.stop();
+	const QString& text = lineEdit()->text();
+	if (text.isEmpty() == false) {
+		completer_.setCompletionPrefix(text);
+		completer_.complete();
+	};
+}
+
+void SearchComboBox::OnCompleterActivated(const QString& text) {
+	setCurrentText(text);
+	setToolTip(text);
+}
---
+++ src/searchcombobox.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QCompleter>
+#include <QComboBox>
+#include <QProxyStyle>
+#include <QTimer>
+
+class QAbstractItemModel;
+
+class SearchComboCompleter : public QCompleter {
+	Q_OBJECT
+public:
+    using QCompleter::QCompleter;
+public slots:
+	void complete(const QRect& rect = QRect());
+};
+
+class SearchComboStyle : public QProxyStyle {
+public:
+    using QProxyStyle::QProxyStyle;
+    int styleHint(StyleHint hint, const QStyleOption* option = nullptr, const QWidget* widget = nullptr, QStyleHintReturn* returnData = nullptr) const override;
+private:
+    const int TOOLTIP_DELAY_MSEC = 50;
+};
+
+class SearchComboBox : public QComboBox {
+    Q_OBJECT
+public:
+    SearchComboBox(QAbstractItemModel* model, QWidget* parent = nullptr);
+private slots:
+	void OnTextEdited();
+	void OnEditTimeout();
+	void OnCompleterActivated(const QString& text);
+private:
+	SearchComboCompleter completer_;
+	QTimer edit_timer_;
+};
