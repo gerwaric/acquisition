4552082245b77bc45b8c72ef0c59aa6d97223c85
GERWARIC <gerwaric@gmail.com>
2024-07-07 11:08:30 -0600

    Add missing parentheses

--- deps/Crashpad/Crashpad-Linux.cmake
+++ deps/Crashpad/Crashpad-Linux.cmake
@@ -4,16 +4,16 @@ set_target_properties(CrashpadHandler PROPERTIES
 
 add_library(CrashpadBase SHARED IMPORTED)
 set_target_properties(CrashpadBase PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libbase.a"
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libbase.a")
 
 add_library(CrashpadCommon SHARED IMPORTED)
 set_target_properties(CrashpadCommon PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libcommon.a"
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libcommon.a")
 
 add_library(CrashpadClient SHARED IMPORTED)
 set_target_properties(CrashpadClient PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libclient.a"
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libclient.a")
 
 add_library(CrashpadUtil SHARED IMPORTED)
 set_target_properties(CrashpadUtil PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libutil.a"
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libutil.a")
