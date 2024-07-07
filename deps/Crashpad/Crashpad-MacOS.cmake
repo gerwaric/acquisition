241bf8ceb0374e5a9554fce6b3a5f99327a15cfd
GERWARIC <gerwaric@gmail.com>
2024-07-07 14:15:57 -0600

    Fix Crashpad targets with precompiled libraries

--- deps/Crashpad/Crashpad-MacOS.cmake
+++ deps/Crashpad/Crashpad-MacOS.cmake
@@ -1,24 +1,40 @@
 add_executable(CrashpadHandler IMPORTED)    
 set_target_properties(CrashpadHandler PROPERTIES
-    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/MacOS/crashpad_handler"
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/MacOS/${CMAKE_SYSTEM_PROCESSOR}/crashpad_handler"
 )
 
-add_library(CrashpadBase SHARED IMPORTED)
-set_target_properties(CrashpadBase PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libbase.a"
-)
+function(setup_macos_target target libname)
 
-add_library(CrashpadCommon SHARED IMPORTED)
-set_target_properties(CrashpadCommon PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libcommon.a"
-)
+    # Point to the precompiled libraries.
+    set(${target}_PREBUILT_LIBS
+        "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/arm64/${libname}.a"
+        "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/x86_64/${libname}.a"
+    )
 
-add_library(CrashpadClient SHARED IMPORTED)
-set_target_properties(CrashpadClient PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libclient.a"
-)
+    # Define the output library, which will be multi-architecture.
+    set(${target}_OUTPUT_LIB "${CMAKE_BINARY_DIR}/libs/${libname}.a")
 
-add_library(CrashpadUtil SHARED IMPORTED)
-set_target_properties(CrashpadUtil PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libutil.a"
-)
+    # Use a custom target to cause lipo to combine the two input libraries.
+    add_custom_target(${target}_MULTIARCH
+        DEPENDS ${${target}_PREBUILT_LIBS}
+        BYPRODUCTS ${${target}_OUTPUT_LIB}
+        COMMAND lipo ${${target}_PREBUILT_LIBS} -create -output ${${target}_OUTPUT_LIB}
+    )
+
+    # Create the target
+    add_library(${target} SHARED IMPORTED)
+
+    # Point the target to the generated multi-architecture library.
+    set_target_properties(${target} PROPERTIES IMPORTED_LOCATION ${${target}_OUTPUT_LIB})
+
+endfunction()
+
+setup_macos_target(CrashpadBase libbase)
+setup_macos_target(CrashpadClient libclient)
+setup_macos_target(CrashpadCommon libcommon)
+setup_macos_target(CrashpadMigOutput libmig_output)
+setup_macos_target(CrashpadUtil libutil)
+
+# Add libraries that were not specified in the top-level CMakeLists.txt
+target_link_libraries(Crashpad PRIVATE CrashpadMigOutput)
+target_link_libraries(Crashpad PRIVATE bsm "-framework AppKit" "-framework Security")
--- deps/Crashpad/Crashpad-Windows.cmake
+++ deps/Crashpad/Crashpad-Windows.cmake
@@ -1,36 +1,27 @@
-add_executable(CrashpadHandler IMPORTED)    
+add_executable(CrashpadHandler IMPORTED)
 set_target_properties(CrashpadHandler PROPERTIES
     IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/Windows/crashpad_handler.exe"
 )
 
-add_library(CrashpadBase SHARED IMPORTED)
-set_target_properties(CrashpadBase PROPERTIES
-    IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/base.lib"
-    IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib"
-    IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/base.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib"
-)
+function(setup_windows_target target libname)
 
-add_library(CrashpadCommon SHARED IMPORTED)
-set_target_properties(CrashpadCommon PROPERTIES
-    IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/common.lib"
-    IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/common.lib"
-    IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/common.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/common.lib"
-)
+    # Create the target
+    add_library(${target} SHARED IMPORTED)
 
-add_library(CrashpadClient SHARED IMPORTED)
-set_target_properties(CrashpadClient PROPERTIES
-    IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/client.lib"
-    IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/client.lib"
-    IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/client.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/client.lib"
-)
+    # Set the correct library locations depending upon the release.
+    set_target_properties(${target} PROPERTIES
+        IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/${libname}.lib"
+        IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/${libname}.lib"
+        IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/${libname}.lib"
+        IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/${libname}.lib"
+    )
 
-add_library(CrashpadUtil SHARED IMPORTED)
-set_target_properties(CrashpadUtil PROPERTIES
-    IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/util.lib"
-    IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib"
-    IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/util.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/util.lib"
-)
+endfunction()
+
+setup_windows_target(CrashpadBase base)
+setup_windows_target(CrashpadClient client)
+setup_windows_target(CrashpadCommon common)
+setup_windows_target(CrashpadUtil util)
+
+# Add libraries that were not specified in the top-level CMakeLists.txt
+target_link_libraries(Crashpad PRIVATE Advapi32)
