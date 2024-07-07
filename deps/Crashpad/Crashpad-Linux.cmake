a2253cb657aea754afafcf6870bcb6ab59a37c46
GERWARIC <gerwaric@gmail.com>
2024-07-07 11:16:27 -0600

    Standardize formatting

--- deps/Crashpad/Crashpad-Linux.cmake
+++ deps/Crashpad/Crashpad-Linux.cmake
@@ -1,19 +1,24 @@
 add_executable(CrashpadHandler IMPORTED)    
 set_target_properties(CrashpadHandler PROPERTIES
-	IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/Linux/crashpad_handler")
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/Linux/crashpad_handler"
+)
 
 add_library(CrashpadBase SHARED IMPORTED)
 set_target_properties(CrashpadBase PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libbase.a")
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libbase.a"
+)
 
 add_library(CrashpadCommon SHARED IMPORTED)
 set_target_properties(CrashpadCommon PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libcommon.a")
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libcommon.a"
+)
 
 add_library(CrashpadClient SHARED IMPORTED)
 set_target_properties(CrashpadClient PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libclient.a")
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libclient.a"
+)
 
 add_library(CrashpadUtil SHARED IMPORTED)
 set_target_properties(CrashpadUtil PROPERTIES
-    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libutil.a")
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libutil.a"
+)
--- deps/Crashpad/Crashpad-Windows.cmake
+++ deps/Crashpad/Crashpad-Windows.cmake
@@ -1,31 +1,36 @@
 add_executable(CrashpadHandler IMPORTED)    
 set_target_properties(CrashpadHandler PROPERTIES
-	IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/Windows/crashpad_handler.exe")
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/Windows/crashpad_handler.exe"
+)
 
 add_library(CrashpadBase SHARED IMPORTED)
 set_target_properties(CrashpadBase PROPERTIES
     IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/base.lib"
     IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib"
     IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/base.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib")
+    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib"
+)
 
 add_library(CrashpadCommon SHARED IMPORTED)
 set_target_properties(CrashpadCommon PROPERTIES
     IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/common.lib"
     IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/common.lib"
     IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/common.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/common.lib")
+    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/common.lib"
+)
 
 add_library(CrashpadClient SHARED IMPORTED)
 set_target_properties(CrashpadClient PROPERTIES
     IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/client.lib"
     IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/client.lib"
     IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/client.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/client.lib")
+    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/client.lib"
+)
 
 add_library(CrashpadUtil SHARED IMPORTED)
 set_target_properties(CrashpadUtil PROPERTIES
     IMPORTED_IMPLIB_DEBUG  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/util.lib"
     IMPORTED_IMPLIB_RELEASE "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/base.lib"
     IMPORTED_IMPLIB_RELWITHDEBINFO "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MDd/util.lib"
-    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/util.lib")
+    IMPORTED_IMPLIB_MINSIZEREL "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Windows/MD/util.lib"
+)
