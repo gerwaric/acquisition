f4a8f34c94214333ed8b9c3639af557d1ff4f858
GERWARIC <gerwaric@gmail.com>
2024-07-07 08:36:49 -0600

    Update cmake for crashpad

---
+++ deps/Crashpad/Crashpad-Linux.cmake
@@ -0,0 +1,19 @@
+add_executable(CrashpadHandler IMPORTED)    
+set_target_properties(CrashpadHandler PROPERTIES
+	IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/Linux/crashpad_handler")
+
+add_library(CrashpadBase SHARED IMPORTED)
+set_target_properties(CrashpadBase PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libbase.a"
+
+add_library(CrashpadCommon SHARED IMPORTED)
+set_target_properties(CrashpadCommon PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libcommon.a"
+
+add_library(CrashpadClient SHARED IMPORTED)
+set_target_properties(CrashpadClient PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libclient.a"
+
+add_library(CrashpadUtil SHARED IMPORTED)
+set_target_properties(CrashpadUtil PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/Linux/libutil.a"
---
+++ deps/Crashpad/Crashpad-MacOS.cmake
@@ -0,0 +1,19 @@
+add_executable(CrashpadHandler IMPORTED)    
+set_target_properties(CrashpadHandler PROPERTIES
+	IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/deps/Crashpad/Bin/MacOS/crashpad_handler")
+
+add_library(CrashpadBase SHARED IMPORTED)
+set_target_properties(CrashpadBase PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libbase.a"
+
+add_library(CrashpadCommon SHARED IMPORTED)
+set_target_properties(CrashpadCommon PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libcommon.a"
+
+add_library(CrashpadClient SHARED IMPORTED)
+set_target_properties(CrashpadClient PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libclient.a"
+
+add_library(CrashpadUtil SHARED IMPORTED)
+set_target_properties(CrashpadUtil PROPERTIES
+    IMPORTED_IMPLIB  "${PROJECT_SOURCE_DIR}/deps/Crashpad/Libraries/MacOS/libutil.a"
