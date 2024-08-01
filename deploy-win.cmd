c408d47b60b0b0487da94b28f13c15533bc60910
GERWARIC <gerwaric@gmail.com>
2024-08-01 16:26:53 -0600

    Copy .exe and .pdb to output directory

--- deploy-win.cmd
+++ deploy-win.cmd
@@ -10,18 +10,23 @@ popd
 rem Setup the Visual Studio environment
 call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
+rem Define the locations of windeployqt and inno setup
 set "WINDEPLOYQT=C:\Qt\6.5.3\msvc2019_64\bin\windeployqt.exe"
 set "ISCC=c:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 
+rem Define the build and deployment directories
 set "BUILD_DIR=.\build\Desktop_Qt_6_5_3_MSVC2019_64bit-Release"
 set "DEPLOY_DIR=%BUILD_DIR%\deploy";
 
 rmdir /S /Q "%DEPLOY_DIR%"
 mkdir "%DEPLOY_DIR%"
 
+rem Build the deployment directory after copying the necessary executables
 copy "%BUILD_DIR%\acquisition.exe" "%DEPLOY_DIR%"
 copy "%BUILD_DIR%\crashpad_handler.exe" "%DEPLOY_DIR%"
-
 "%WINDEPLOYQT%" "%DEPLOY_DIR%\acquisition.exe" --release --compiler-runtime --dir "%DEPLOY_DIR%"
 
+rem Create the installer and copy the exe and pdb for uploading to BugSplat
 "%ISCC%" /DBUILD_DIR="%BUILD_DIR%" /DDEPLOY_DIR="%DEPLOY_DIR%" installer.iss
+copy "%BUILD_DIR%\acquisition.exe" .\Output
+copy "%BUILD_DIR%\acquisition.pdb" .\Output
