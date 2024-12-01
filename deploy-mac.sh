df3339391da21e7c0a3ecb26701b7305f39a0124
GERWARIC <gerwaric@gmail.com>
2024-11-30 22:27:01 -0800

    Update deploy-mac.sh for Qt 6.8.0

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MACDEPLOYQT=~/Qt/6.5.3/macos/bin/macdeployqt
+MACDEPLOYQT=~/Qt/6.8.0/macos/bin/macdeployqt
 
-TARGET=./build/Qt_6_5_3_for_macOS-Release
+TARGET=./build/Qt_6_8_0_for_macOS-Release
 
 pushd $TARGET
 
