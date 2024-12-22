9f89fdbbdaeedb49c60cbf587c2d76ed482fb5e1
GERWARIC <gerwaric@gmail.com>
2024-12-22 17:27:45 -0500

    Add copyright statements

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright (C) 2014-2024 Acquisition Contributors
+#
+# This file is part of Acquisition.
+#
+# Acquisition is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Acquisition is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Acquisition.  If not, see <http://www.gnu.org/licenses/>.
+
 MACDEPLOYQT=~/Qt/6.8.0/macos/bin/macdeployqt
 
 TARGET=./build/Qt_6_8_0_for_macOS-Release
--- deploy-win.cmd
+++ deploy-win.cmd
@@ -1,3 +1,20 @@
+rem Copyright (C) 2014-2024 Acquisition Contributors
+rem
+rem This file is part of Acquisition.
+rem
+rem Acquisition is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem 
+rem Acquisition is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem 
+rem You should have received a copy of the GNU General Public License
+rem along with Acquisition.  If not, see <http://www.gnu.org/licenses/>.
+
 echo -------------------------------------------------------------
 
 setlocal
