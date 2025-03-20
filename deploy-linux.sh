a476264d3518fd393e5cd091370ea37167c71e05
GERWARIC <gerwaric@gmail.com>
2025-03-19 18:50:29 -0700

    Update deploy-linux.sh

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -17,11 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Acquisition.  If not, see <http://www.gnu.org/licenses/>.
 
-export QMAKE=${HOME}/Qt/6.8.1/gcc_64/bin/qmake
+export QMAKE=${HOME}/Development/Qt/6.8.2/gcc_64/bin/qmake
 
-LINUXDEPLOY=${HOME}/bin/linuxdeploy-x86_64.AppImage
+LINUXDEPLOY=${HOME}/Development/linuxdeploy/linuxdeploy-x86_64.AppImage
 PROJECT_DIR=${PWD}
-BUILD_DIR=${PWD}/build/Desktop_Qt_6_8_1-Release
+BUILD_DIR=${PWD}/build/Desktop_Qt_6_8_2-Release
 
 # Take the version string from version_defines.h
 export LINUXDEPLOY_OUTPUT_VERSION=`grep APP_VERSION_STRING ${BUILD_DIR}/version_defines.h | cut -d'"' -f2`
