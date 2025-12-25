36f00c8e62594f750a75afab5dba0430f0b8322d
GERWARIC <gerwaric@gmail.com>
2025-12-25 15:19:46 -0800

    Remove "Hello, World!" sentry logging.

--- src/main.cpp
+++ src/main.cpp
@@ -192,11 +192,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    sentry_capture_event(sentry_value_new_message_event(
-        /*   level */ SENTRY_LEVEL_INFO,
-        /*  logger */ "custom",
-        /* message */ "Hello World!"));
-
     // Starting the application creates and shows the login dialog.
     QTimer::singleShot(0, &app, [&] { app.Start(appDataDir); });
 
