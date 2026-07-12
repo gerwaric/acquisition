892e86b23d1ddd1946bf197998a0f82529af034e
GERWARIC <gerwaric@gmail.com>
2026-07-12 08:01:03 -0500

    fix: detach LogPanel sinks on teardown

--- src/ui/logpanel.h
+++ src/ui/logpanel.h
@@ -6,6 +6,10 @@
 #include <QObject>
 
 #include <atomic>
+#include <memory>
+
+#include <spdlog/logger.h>
+#include <spdlog/sinks/sink.h>
 
 class MainWindow;
 class QPushButton;
@@ -20,6 +24,7 @@ class LogPanel : public QObject
     Q_OBJECT
 public:
     LogPanel(MainWindow *window, Ui::MainWindow *ui);
+    ~LogPanel() override;
     virtual bool isValid() { return true; }
 public slots:
     void TogglePanelVisibility();
@@ -29,6 +34,10 @@ private:
 
     QPushButton *m_status_button;
     QTextEdit *m_output;
+
+    std::shared_ptr<spdlog::logger> m_logger;
+    spdlog::sink_ptr m_panel_sink;
+    spdlog::sink_ptr m_callback_sink;
 
     std::atomic_uint m_num_errors{0};
     std::atomic_uint m_num_warnings{0};
